76a3673523a8e6519bcec6080dcb4971f1582249
Fahim <littlefahim2000@gmail.com>
2023-05-06 02:35:58 +0600

    message

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -5,26 +5,26 @@ function Projects() {
   return (
     <div id='projects' className='w-full my-16'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
-        <h1 className='py-4 my-4 tracking-wide'>Project&#39s I have built recently</h1>
+        <h1 className='py-4 my-4 tracking-wide'>Projects I have built recently</h1>
         <div className='grid md:grid-cols-2 mt-16 gap-8'>
           <ProjectItem
             title='Google Search Engine'
-            bgImg={"/../public/projects/google.jpg"}
+            bgImg={"/projects/google.jpg"}
             url='/'
           />
           <ProjectItem
             title='Google Search Engine'
-            bgImg={"/../public/projects/google.jpg"}
+            bgImg={"/projects/google.jpg"}
             url='/'
           />
           <ProjectItem
             title='Whatsapp Clone'
-            bgImg={"/../public/projects/google.jpg"}
+            bgImg={"/projects/google.jpg"}
             url='/'
           />
           <ProjectItem
             title='Netflix Like'
-            bgImg={"/../public/projects/google.jpg"}
+            bgImg={"/projects/google.jpg"}
             url='/'
           />
         </div>
